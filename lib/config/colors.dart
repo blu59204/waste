a509ec5f8daaddc5062eec000bf4aa9d121b857e
Preetham V <1ew22ai035@ewit.edu.in>
2025-10-18 23:48:27 +0530

    Create color palette in colors.dart

---
+++ lib/config/colors.dart
@@ -0,0 +1,11 @@
+// Color Palette
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF7CB342);
+  static const Color accentGold = Color(0xFFFFB74D);
+  static const Color lightCream = Color(0xFFF5F0E8);
+  static const Color warmBeige = Color(0xFFD4A574);
+  static const Color darkBrown = Color(0xFF6D4C41);
+}
